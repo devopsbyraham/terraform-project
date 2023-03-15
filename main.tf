e2e05c61b0a157314facbe286c782baa313aadb9
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-03-15 23:44:58 +0530

    terracommits

---
+++ main.tf
@@ -0,0 +1,66 @@
+#this file consists of code for instances and sg
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  ami             = "ami-0a2457eba250ca23d"
+  instance_type   = "t2.micro"
+  key_name        = "terrakeypair"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "ap-south-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-0a2457eba250ca23d"
+  instance_type   = "t2.micro"
+  key_name        = "terrakeypair"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "ap-south-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "server-2"
+  }
+}
+
+resource "aws_security_group" "three" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
