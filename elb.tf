f606da1cae9ada1b46ee988ae1454cc43ab44c72
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-05-03 16:29:53 +0530

    newcomits

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "raham-terraform-elb"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
     instance_port     = 80
@@ -10,8 +10,8 @@ resource "aws_elb" "bar" {
   }
 
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
     timeout             = 5
     target              = "HTTP:80/"
     interval            = 30
@@ -25,11 +25,11 @@ resource "aws_elb" "bar" {
   }
 }
 
-resource "aws_rds_cluster" "default" {
+ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  availability_zones      = ["us-east-1a", "us-east-1b"]
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "Rahamshaik#444555"
