2dd871fd18e1f34fa2716c2ae46dba1ad9e91696
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-08-08 21:32:44 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "raham-terraform-elb"
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  availability_zones = ["eu-west-3a", "eu-west-3b"]
 
   listener {
     instance_port     = 80
@@ -29,9 +29,9 @@ resource "aws_elb" "bar" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["us-east-1a", "us-east-1b"]
+  availability_zones      = ["eu-west-3a", "eu-west-3b"]
   database_name           = "mydb"
-  master_username         = "foo"
+  master_username         = "raham"
   master_password         = "Rahamshaik#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
