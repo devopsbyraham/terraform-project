0368089f1195da54316ad6c1aadd728800f01387
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-09-12 23:04:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "eu-west-3"
-access_key = ""
-secret_key = ""
 }
 
 resource "aws_instance" "one" {
   ami             = "ami-0b8b5288592eca360"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "1045ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3a"
   user_data       = <<EOF
@@ -27,7 +25,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0b8b5288592eca360"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "1045ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3b"
   user_data       = <<EOF
@@ -46,7 +44,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-0b8b5288592eca360"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "1045ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3a"
   tags = {
@@ -57,7 +55,7 @@ resource "aws_instance" "three" {
 resource "aws_instance" "four" {
   ami             = "ami-0b8b5288592eca360"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "1045ambatch"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3b"
   tags = {
