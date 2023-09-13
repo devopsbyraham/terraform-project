b71300d914c6e4e10d48dec9c4d3bfc2ae510837
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-09-13 09:33:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "eu-west-3"
+region = "ap-southeast-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0b825ad86ddcfb907"
   instance_type   = "t2.micro"
-  key_name        = "1045ambatch"
+  key_name        = "singaporekey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3a"
+  availability_zone = "ap-southeast-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -23,11 +23,11 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0b825ad86ddcfb907"
   instance_type   = "t2.micro"
-  key_name        = "1045ambatch"
+  key_name        = "singaporekey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3b"
+  availability_zone = "ap-southeast-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -42,22 +42,22 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0b825ad86ddcfb907"
   instance_type   = "t2.micro"
-  key_name        = "1045ambatch"
+  key_name        = "singaporekey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3a"
+  availability_zone = "ap-southeast-1a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0b825ad86ddcfb907"
   instance_type   = "t2.micro"
-  key_name        = "1045ambatch"
+  key_name        = "singaporekey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3b"
+  availability_zone = "ap-southeast-1b"
   tags = {
     Name = "app-server-2"
   }
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterrttsda7gsywgys7889900prodenvgshj"
+  bucket = "rahamterraserverbucketoo99"
 }
 
 resource "aws_iam_user" "seven" {
@@ -103,7 +103,7 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "eu-west-3a"
+ availability_zone = "ap-southeast-1a"
   size = 40
   tags = {
     Name = "ebs-001"
