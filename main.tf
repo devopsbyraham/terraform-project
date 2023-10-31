177c2e5bbee375172f5c14a542cd9b027ab38d91
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2023-10-31 07:19:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucketoo99"
+  bucket = "devopsbyrahamterraserverbucketoo9988"
 }
 
 resource "aws_iam_user" "seven" {
